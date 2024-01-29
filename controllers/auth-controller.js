a80aa8343a35979bb55166eafcaf60d1e230e9ab
Nazar-Odno <114081040+Nazar-Odno@users.noreply.github.com>
2024-01-29 03:51:34 +0100

    homework

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -93,16 +93,11 @@ const ChangeAvatar = async (req, res) => {
     res.status(400).json({ message: "No file uploaded" });
   }
 
-  // console.log(req.user);
   let { avatarURL, _id } = req.user;
-  // console.log("qqq", Jimp);
-  // console.log(req.file);
-
   //перемещаем файл
   const { path: oldPath, filename } = req.file;
 
   Jimp.read(oldPath, (err, image) => {
-    // console.log(image);
     if (err) throw err;
 
     image.resize(250, 250); // изменяем ширину на 800 пикселей, а высоту автоматически
@@ -110,12 +105,10 @@ const ChangeAvatar = async (req, res) => {
     const newPath = path.join(avatarPath, filename);
     image.write(newPath); // сохраняем измененное изображение
   });
-  // oldPath
-  // await fs.rename(oldPath, newPath);
+
   //создаем новый путь к перемещенному файлу
   const poster = path.join("avatars", filename);
   avatarURL = poster;
-  // console.log(poster);
   const result = await User.findByIdAndUpdate(_id, { avatarURL });
 
   res.status(200).json(result.avatarURL);
